82f713775d8ef1a69699c0c6b9d7ca60815945b1
AstrOmewa <omewa242@gmail.com>
2025-02-24 17:07:02 +0300

    modified:   README.md 	renamed:    new/KPolyakov/24/5390/24.5392.txt -> new/KPolyakov/24/5389/24.5389.txt 	new file:   new/KPolyakov/24/5389/Q_5389.txt 	modified:   new/KPolyakov/24/5389/S_5389.py 	new file:   new/KPolyakov/24/5390/24.5390.txt 	new file:   new/KPolyakov/24/5390/Q_5390.txt 	modified:   new/KPolyakov/24/5390/S_5390.py 	deleted:    new/Kompege/24/.vscode/launch.json 	deleted:    new/Kompege/24/.vscode/tasks.json 	renamed:    new/Kompege/24/24.12_19719.txt -> new/Kompege/24/19719/24.19719.txt 	renamed:    new/Kompege/24/Q_19719.txt -> new/Kompege/24/19719/Q_19719.txt 	renamed:    new/Kompege/24/S_19719.cpp -> new/Kompege/24/19719/S_19719.cpp 	renamed:    new/Kompege/24/S_19719.exe -> new/Kompege/24/19719/S_19719.exe 	renamed:    new/KPolyakov/24/redistribute.py -> new/Kompege/24/redistribute.py

--- new/KPolyakov/24/5389/S_5389.py
+++ new/KPolyakov/24/5389/S_5389.py
@@ -1,7 +1,7 @@
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-with open('24.5392.txt') as f:
+with open('24.5389.txt') as f:
     s = f.readline().replace('B', 'A').replace('C', 'A').replace('2', '1').replace('3', '1')
 
     s = s.replace('A11', '*').replace('A', ' ').replace('1', ' ')
--- new/KPolyakov/24/5390/S_5390.py
+++ new/KPolyakov/24/5390/S_5390.py
@@ -1,7 +1,7 @@
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-with open('24.5392.txt') as f:
+with open('24.5390.txt') as f:
     s = f.readline().replace('B', 'A').replace('C', 'A').replace('2', '1').replace('3', '1')
 
     s = s.replace('11A', '*').replace('A', ' ').replace('1', ' ')
