8968c3e5f98ec2b18142eb343d7f26d514391fb1
asdashd3 <akllsdg@videotoptop.com>
2025-04-21 11:15:40 +0300

    last

--- ex210425/1.py
+++ ex210425/1.py
@@ -1,22 +1,24 @@
 from turtle import *
 
 lt(90)
-screensize(2000, 2000)
+screensize(3000, 3000)
 color('red','blue')
-k = 10
-speed(1000)
+k = 15
+tracer(0)
 begin_fill()
 for i in range(2):
-    fd(3*k)
+    fd(7*k)
     rt(90)
     fd(12*k)
     rt(90)
 end_fill()
-fd(3*k)
+
+up()
+fd(7*k)
 rt(90)
 fd(12*k)
 rt(90)
-up()
+
 fd(4*k)
 rt(90)
 fd(6*k)
@@ -29,22 +31,23 @@ for i in range(2):
     fd(77*k)
     rt(90)
 end_fill()
-
-# for x in range(-100,100):
-#     for y in range(-90*k, 10*k, k):
-#         pu()
-#         goto(x,y)
-#         pd()
+up()
+# for x in range(-10,10):
+#     for y in range(-10,10):
+#         up()
+#         goto(x*k,y*k)
+#         down()
 #         dot(3)
 
 canvas = getcanvas()
 
 c = 0
-for x in range(-100,100):
-    for y in range(-100,100):
+for x in range(-90,30):
+    for y in range(-90,30):
         s = canvas.find_overlapping(x*k,y*k,x*k,y*k)
+        print(s)
         if s!=():
             c+=1
 
-print(c)
+print(c, 84*78+13*8-7*4)
 done()
