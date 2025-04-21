6209b5795c2775f70456e3246624ea5eeddf0990
asdashd3 <akllsdg@videotoptop.com>
2025-04-21 11:00:05 +0300

    Все 7 номеров

--- ex210425/4.py
+++ ex210425/4.py
@@ -3,3 +3,16 @@ from string import *
 ascii = [chr(i) for i in range(10000)]
 
 with open('4.txt') as f:
+    s = f.readline()
+    l = 0
+    mlen = 0
+    str = []
+
+    for r in range(1,len(s)):
+        if s[r]<s[r-1]:
+            mlen = max(mlen, r - l + 1)
+        else:
+            str.append(s[l:r])
+            l = r
+    print([x for x in str if len(x)==6])
+    print((max(str, key=len)))
